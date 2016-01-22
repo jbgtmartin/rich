0ce44c4f6179e44d88116b546336f9ffaab21cbc
Jean-Baptiste Martin <jbgtmartin@gmail.com>
2016-01-22 16:40:58 +0100

    Affichage des données de chaque site dans le dashboard

--- frontend/app/scripts/controllers/dashboard.js
+++ frontend/app/scripts/controllers/dashboard.js
@@ -9,16 +9,27 @@
  */
 angular.module('frontendApp')
   .controller('DashboardCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.tests = ['dheikezpno', 'dbezopb'];
-    $scope.cake = "cakephp";
+
     $http({
 	  method: 'GET',
-	  url: base_url + 'backend/posts'
+	  url: base_url + 'backend2/api/websites/get/'
 	}).then(function successCallback(response) {
-	    $scope.cake = response;
+	    $scope.all_websites = response.data;
+	    showItem(response.data[0]._id);
 	  }, function errorCallback(response) {
 	    // called asynchronously if an error occurs
 	    // or server returns response with an error status.
 	  });
+
+	function showItem(id) {
+		$scope.active = $scope.all_websites.filter(
+		    function(data){return data._id == id}
+		)[0];
+		$scope.websites_list_item_class = "active";
+	}
+
+	$scope.changeWebsite = function(id) {
+		showItem(id)
+	};
 
   }]);
